1b69de8815925f01b689844526cd6abeb6533d6d
krystella <krystellarattan@hotmail.com>
2022-05-21 10:03:19 -0400

    added Exercise_1

---
+++ Coursework/Exercise1.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 21 08:19:24 2022
+
+@author: Krystella
+
+Course: Scientific Computing with Python
+
+Title: Worked Example - hello world
+
+"""
+
+# Q. Write a program that uses a print statement to say 'hello world
+
+print("Hello world")
+
